6efe167ab77f3263b08cd6799c0d6df32ed53ce5
Julien Ponge <jponge@redhat.com>
2022-10-05 09:54:29 +0200

    Scratchpad file

---
+++ mutiny-scratchpad/Scratchpad.java
@@ -0,0 +1,33 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS io.smallrye.reactive:mutiny:1.7.0
+
+import java.io.IOException;
+import java.time.Duration;
+import io.smallrye.mutiny.Multi;
+
+public class Scratchpad {
+
+    public static void main(String... args) {
+        
+        var stream = Multi.createFrom().generator(() -> 0, (state, emitter) -> {
+            if (state < 100) {
+                emitter.emit(state);
+                return state + 1;
+            } else {
+                emitter.complete();
+                return 0;
+            }
+        });
+
+        stream
+            .onItem().transform(n -> "[" + n + "]")
+            .onFailure(IOException.class).retry().atMost(5)
+            .ifNoItem().after(Duration.ofSeconds(5)).fail()
+            .subscribe().with(
+                item -> System.out.println(">>> " + item),
+                Throwable::printStackTrace,
+                () -> System.out.println("Done")
+            );
+
+    }
+}
